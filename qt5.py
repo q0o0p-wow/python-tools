cbc4681fb7a8e7e4231a3ba2642a460047bc4178
q0o0p <2944518598@qq.com>
2021-05-22 00:43:58 +0800

    used tools that adb management documents

--- qt5.py
+++ qt5.py
@@ -2,9 +2,11 @@ import sys
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import os
-
+import subprocess
 from PyQt5 import QtCore, QtGui, QtWidgets
 import requests
+
+
 
 class MyTest(QWidget):
 
@@ -21,11 +23,6 @@ class MyTest(QWidget):
         # self.setStyleSheet("#MainWindow{border-image:url(w.png);}")
         # 设置窗口透明度
         self.setStyleSheet ("color:rgb(248,248,255);font-size:16px;background-color:rgb(54,54,54);border:0.5px groove gray;border-radius:8px;padding:2px 2px;");
-        # self.setStyleSheet("#MainWindow{background-color:black}")
-                
-
-         # 设置窗口背景透明        
-        # self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 
         self.label = QLabel(self)
         self.label.setText("q0o0p")
@@ -41,6 +38,12 @@ class MyTest(QWidget):
         self.btn_startApp= QPushButton("start app")
         self.btn_readActivity= QPushButton("activity")
         self.btn_package= QPushButton("all package")
+        self.btn_ip= QPushButton("IP Address")
+        self.btn_download= QPushButton("Download-> AndroidTool")
+        self.btn_push= QPushButton("this-> tools")
+        self.btn_read_files= QPushButton("read list files")
+        self.btn_read_download= QPushButton("read download")
+        self.btn_get_clipboard= QPushButton("get clipboard")
         self.btn_package.setStyleSheet("color:rgb(108,166,205)")
         self.text = QTextEdit()
         #信号于槽
@@ -58,7 +61,13 @@ class MyTest(QWidget):
         self.btn_decimal.clicked.connect(self.decimal)
         self.btn_readActivity.clicked.connect(self.read_activity)
         self.btn_package.clicked.connect(self.package)
+        self.btn_ip.clicked.connect(self.phone_ip)
+        self.btn_download.clicked.connect(self.download_file)
+        self.btn_push.clicked.connect(self.adb_push)
+        self.btn_read_download.clicked.connect(self.read_download)
+        self.btn_read_files.clicked.connect(self.read_files)
         self.btn_startApp.clicked.connect(self.startApp)
+        self.btn_get_clipboard.clicked.connect(self.get_clipboard)
         #布局嵌套
         wlayout = QVBoxLayout(self) #全局布局
         hlayout = QHBoxLayout() #行局部布局
@@ -87,6 +96,12 @@ class MyTest(QWidget):
 
         btn1.addWidget(self.btn_readActivity)
         btn1.addWidget(self.btn_package)
+        btn1.addWidget(self.btn_ip)
+        btn1.addWidget(self.btn_download)
+        btn1.addWidget(self.btn_push)
+        btn1.addWidget(self.btn_read_download)
+        btn1.addWidget(self.btn_read_files)
+        btn1.addWidget(self.btn_get_clipboard)
 
         textView2.addWidget(self.print_activity)
         textView2.addWidget(self.print_cmd)
@@ -140,6 +155,75 @@ class MyTest(QWidget):
         except Exception as e:
             print(e)
 
+    def read_download(self):
+        try:
+            android_path='/storage/emulated/0/Download/'
+            cmd='adb shell ls -al '+android_path
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+            for info in p.communicate():
+                self.text.append(info.decode())
+            print(p)
+            # self.text.append(p.returncode)
+        except Exception as e:
+            print(e)
+    def adb_push(self):
+        try:
+            android_path='/storage/emulated/0/q0o0p/'
+            this_path=os.getcwd() # this path(not file name)
+            file_name= os.path.basename(__file__)
+            cmd="adb push "+this_path+'/'+file_name+' '+android_path
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+            for info in p.communicate():
+                self.text.append(info.decode())
+            self.print.append("push success!...")
+            cmd=""" adb shell su -c "mv /storage/emulated/0/q0o0p/qt5.py /data/data/com.termux/files/home/tools/" """
+            mv = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+            self.print.append('mv success!...') 
+        except Exception as e:
+            print(e)
+    def phone_ip(self):
+        try:
+            files=self.appPackage.text()
+            cmd='adb shell ifconfig '
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+            for info in p.communicate():
+                self.print.append(info.decode())
+        except Exception as e:
+            print(e)
+    def get_clipboard(self):
+        try:
+            files=self.appPackage.text()
+            cmd=""" adb shell su -c "cat /data/data/com.sohu.inputmethod.sogou.xiaomi/files/sogou_clipboard" """
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+            for info in p.communicate():
+                self.print.append(info.decode())          
+        except Exception as e:
+            print(e)
+    def read_files(self):
+        try:
+            files=self.appPackage.text()
+            android_path='/storage/emulated/0/'+files.replace(' ', '') +'/'
+            cmd='adb shell ls -al '+android_path
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+            for info in p.communicate():
+                self.text.append(info.decode())
+            print(p)
+        except Exception as e:
+            print(e)
+        
+
+    def download_file(self):
+        try:
+            android_path='/storage/emulated/0/Download/'
+            pc_path='F:/AndroidTools/'
+            file=self.appPackage.text()
+            cmd='adb pull '+android_path+file+''+pc_path
+            p = subprocess.Popen(cmd, shell=True)
+            for info in p.communicate():
+                self.text.append(info.decode())
+        except Exception as e:
+            print (e)
+
     def decimal(self):
         try:
             code=self.lineEdit.text()
@@ -183,6 +267,7 @@ class MyTest(QWidget):
         except Exception :
             print('http is null')
         
+
 
 
 
